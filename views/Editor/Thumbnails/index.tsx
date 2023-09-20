feaf6b833eb1aea133eae4e85e2270d2546f45c4
ljq0226 <1035821043@qq.com>
2023-09-20 09:56:28 +0800

    chore:style

--- views/Editor/Thumbnails/index.tsx
+++ views/Editor/Thumbnails/index.tsx
@@ -1,6 +1,20 @@
+import { Button } from '@/components/ui/button'
+import { Icon } from '@/assets/icons'
+
 function Thumbnails() {
   return (
-    <div className="w-[160px]">Thumbnails</div>
+    <div className="w-[160px] flex flex-col ">
+      <div className='flex-center h-10 border-b border-border'>
+        <Button className='flex-center flex-1'>
+          <Icon icon='heroicons:plus' height={20} />
+          <span>新增幻灯片</span>
+        </Button>
+        <div className='w-[30px] h-full border-x border-border flex-center'>
+          <Icon icon={'mingcute:down-fill'}></Icon>
+        </div>
+      </div>
+
+    </div>
   )
 }
 
